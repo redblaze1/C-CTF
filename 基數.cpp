58581242d0793d60976f714333ae35099635a3db
redblaze1 <cccvvv2108@gmail.com>
2017-10-11 11:03:55 +0800

    Add files via upload

---
+++ 基數.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+ int main(){
+ 	int num;
+ 	
+ 	std::cin >> num;
+ 	
+ 	if ( num%2 == 1 ){
+ 		
+		 std::cout << "為基數" << std::endl; 
+	 }
+	if ( num%2 == 0)
+	 std::cout << "為偶數" << std::endl; 
+ }
